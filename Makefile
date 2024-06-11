7cd4e9c423c7f3a63a843b371af33a813b6cc84f
hiro <huyp26102000@gmail.com>
2024-06-11 23:44:00 +0700

    update check api key openai for dallE model

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+init:
+	python3 -m venv main_env \
+	&& main_env/bin/pip install -r requirements.txt
 run:
 	./main_env/bin/python app.py
--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from dotenv import load_dotenv
 import requests
 import time
 import math
+import numpy as np
+import openai
 
 load_dotenv()
 
 def round_down(x, decimals=5):
     multiplier = 10 ** decimals
     return math.floor(x * multiplier) / multiplier
-def sendTeleMessage(message):
+def send_tele_message(message):
     try:
         print(f"https://api.telegram.org/bot{os.getenv('telegram_token')}/sendMessage")
         response = requests.post(f"https://api.telegram.org/bot{os.getenv('telegram_token')}/sendMessage", json={
@@ -28,15 +30,20 @@ def sendTeleMessage(message):
     except:
         print("Send tele fail")
 
-acceptedResp = """{"message":"Synapse name '' not found. Available synapses ['Synapse', 'ImageGenerating', 'TextGenerating']"}"""
-while (True):
-    metagraph = bt.metagraph(int(os.getenv('netuid')), network=os.getenv('network'))
-    coldkeys = os.getenv('track_coldkey').split(",")
-    netColdkeys = metagraph.coldkeys
-    nodeUrl = metagraph.axons
-    incentiveData = metagraph.I
-    searchIndex = {}
-    nodeData = {}
+# while (True):
+metagraph = bt.metagraph(int(os.getenv('netuid')), network=os.getenv('network'))
+coldkeys = os.getenv('track_coldkey').split(",")
+openai_api_key=os.getenv('openai_api_key').split(",")
+netColdkeys = metagraph.coldkeys
+nodeUrl = metagraph.axons
+
+incentive_data = metagraph.incentive
+array_incentive_data = incentive_data.numpy()
+sorted_incentive_data = np.sort(array_incentive_data)
+print(sorted_incentive_data)
+search_index = {}
+nodeData = {}
+while(True):
     for coldkey in coldkeys:
         indices = []
         for i, value in enumerate(netColdkeys):
@@ -46,18 +53,18 @@ while (True):
                 print(port, ip)
                 try:
                     response = requests.get(f"http://{ip}:{port}", timeout=8)
-                    if (response.status_code == 200):
+                    if (len(response.text) > 5):
                         indices.append({
                             "uid": i,
                             "running": True,
-                            "incentive": incentiveData[i],
+                            "incentive": incentive_data[i],
                             "host": f"{ip}:{port}"
                         })
                     else:
                         indices.append({
                             "uid": i,
                             "running": False,
-                            "incentive": incentiveData[i],
+                            "incentive": incentive_data[i],
                             "host": f"{ip}:{port}"
                         })
                 except requests.exceptions.RequestException as e:
@@ -65,16 +72,33 @@ while (True):
                     indices.append({
                         "uid": i,
                         "running": False,
-                        "incentive": incentiveData[i],
+                        "incentive": incentive_data[i],
                         "host": f"{ip}:{port}"
                     })
-        searchIndex[coldkey] = indices
+        search_index[coldkey] = indices
     formatedMessage = ""
     for coldkey in coldkeys:
         fmColdkey = ""
-        for node in searchIndex[coldkey]:
+        for node in search_index[coldkey]:
+            print(node["incentive"], str(node["incentive"]))
+            print(np.where(sorted_incentive_data == int(node["incentive"])))
             fmColdkey  = fmColdkey + f"""
-<b>{node["uid"]}</b>:{round_down(node["incentive"], 5)} <b>{"OK" if node["running"] == True else "Not Running @hiro_trk @tian_ng" }</b> <code>{node["host"]}</code>"""
+<b>{node["uid"]}</b>:{round_down(node["incentive"], 5)} <b>{"" if node["running"] == True else f"Not Running {os.getenv('telegram_tag_user')}" }</b>\n<code>{node["host"]}</code>"""
         formatedMessage = formatedMessage + f"\n<b>{coldkey}</b>\n" + f"{fmColdkey}"
-    sendTeleMessage(formatedMessage)
+    for apikey in openai_api_key:
+        try:
+            openai.api_key = apikey
+            print(openai.api_key)
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "user", "content": "hello"}
+                ],
+                max_tokens=10
+            )
+            print(response)
+        except:
+            print(f"API Error")
+            formatedMessage = formatedMessage + f"\n{apikey} {os.getenv('telegram_tag_user')}"
+    send_tele_message(formatedMessage)
     time.sleep(300)
